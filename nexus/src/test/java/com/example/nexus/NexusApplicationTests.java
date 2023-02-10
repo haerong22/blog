9d0b99d6ad47531c75ad141400a77635d3f733fb
haerong22 <haerong22@naver.com>
2023-02-10 22:07:17 +0900

    #4 nexus: custom repository

---
+++ nexus/src/test/java/com/example/nexus/NexusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
