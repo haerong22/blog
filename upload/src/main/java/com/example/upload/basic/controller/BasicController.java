527565dc7cfed94ce1ef3103f3f6a6e61e5a4aee
haerong22 <haerong22@naver.com>
2023-02-24 20:25:05 +0900

    #7 upload: basic upload - fetch

--- upload/src/main/java/com/example/upload/basic/controller/BasicController.java
+++ upload/src/main/java/com/example/upload/basic/controller/BasicController.java
@@ -4,17 +4,11 @@ package com.example.upload.basic.controller;
 import com.example.upload.basic.service.BasicService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,11 +21,12 @@ public class BasicController {
         return "basic";
     }
 
+    @ResponseBody
     @PostMapping("/basic")
-    public String saveFile(Model model, @RequestParam MultipartFile file) {
+    public String saveFile(@RequestParam("file") MultipartFile file,
+                           @RequestParam("desc") String description) {
         basicService.saveFile(file);
-        model.addAttribute("result", "업로드 성공!!");
-        return "basic";
+        return "업로드 성공!! - 파일 이름: " + file.getOriginalFilename() + ", 파일 설명: " + description;
     }
 
 }
