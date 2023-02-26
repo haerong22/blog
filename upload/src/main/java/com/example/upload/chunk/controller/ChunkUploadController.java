def6390d28c2695dbde6bb05234064e77b2372ac
haerong22 <haerong22@naver.com>
2023-02-26 16:30:32 +0900

    #7 upload: chunk upload - resume upload

--- upload/src/main/java/com/example/upload/chunk/controller/ChunkUploadController.java
+++ upload/src/main/java/com/example/upload/chunk/controller/ChunkUploadController.java
@@ -5,10 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -31,11 +28,18 @@ public class ChunkUploadController {
     }
 
     @ResponseBody
-    @PostMapping("/chunk/upload")
+    @GetMapping("/chunk/upload/{key}")
+    public ResponseEntity<?> getLastChunkNumber(@PathVariable String key) {
+        return ResponseEntity.ok(chunkUploadService.getLastChunkNumber(key));
+    }
+
+    @ResponseBody
+    @PostMapping("/chunk/upload/{key}")
     public ResponseEntity<String> chunkUpload(@RequestParam("chunk") MultipartFile file,
                                               @RequestParam("chunkNumber") int chunkNumber,
-                                              @RequestParam("totalChunks") int totalChunks) throws IOException {
-        boolean isDone = chunkUploadService.chunkUpload(file, chunkNumber, totalChunks);
+                                              @RequestParam("totalChunks") int totalChunks,
+                                              @PathVariable String key) throws IOException {
+        boolean isDone = chunkUploadService.chunkUpload(file, chunkNumber, totalChunks, key);
 
         return isDone ?
                 ResponseEntity.ok("File uploaded successfully") :
--- upload/src/main/java/com/example/upload/chunk/service/ChunkUploadService.java
+++ upload/src/main/java/com/example/upload/chunk/service/ChunkUploadService.java
@@ -11,21 +11,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
 public class ChunkUploadService {
-    public boolean chunkUpload(MultipartFile file, int chunkNumber, int totalChunks) throws IOException {
+    public boolean chunkUpload(MultipartFile file, int chunkNumber, int totalChunks, String key) throws IOException {
         String uploadDir = "video";
+        String tempDir = "video/" + key;
 
-        File dir = new File(uploadDir);
+        File dir = new File(tempDir);
         if (!dir.exists()) {
             dir.mkdirs();
         }
 
         String filename = file.getOriginalFilename() + ".part" + chunkNumber;
 
-        Path filePath = Paths.get(uploadDir, filename);
+        Path filePath = Paths.get(tempDir, filename);
         Files.write(filePath, file.getBytes());
 
         if (chunkNumber == totalChunks-1) {
@@ -34,14 +36,27 @@ public class ChunkUploadService {
             Path outputFile = Paths.get(uploadDir, outputFilename);
             Files.createFile(outputFile);
             for (int i = 0; i < totalChunks; i++) {
-                Path chunkFile = Paths.get(uploadDir, file.getOriginalFilename() + ".part" + i);
+                Path chunkFile = Paths.get(tempDir, file.getOriginalFilename() + ".part" + i);
                 Files.write(outputFile, Files.readAllBytes(chunkFile), StandardOpenOption.APPEND);
-                Files.delete(chunkFile);
             }
+            deleteDirectory(Paths.get(tempDir));
             log.info("File uploaded successfully");
             return true;
         } else {
             return false;
         }
+    }
+
+    private void deleteDirectory(Path directory) throws IOException {
+        try (Stream<Path> walk = Files.walk(directory)){
+            walk.map(Path::toFile).forEach(File::delete);
+        }
+        Files.delete(directory);
+    }
+
+    public int getLastChunkNumber(String key) {
+        Path temp = Paths.get("video", key);
+        String[] list = temp.toFile().list();
+        return list == null ? 0 : Math.max(list.length-2, 0);
     }
 }
