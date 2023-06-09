4c87600c3f791eda6d46232dcd51c852ba3120bd
haerong22 <haerong22@naver.com>
2023-06-09 21:37:59 +0900

    #7 upload: nginx rtmp module - thumbnail

--- upload/nginx/start.sh
+++ upload/nginx/start.sh
@@ -2,4 +2,4 @@
 
 docker build -t nginx-rtmp .
 docker rm nginx-rtmp -f
-docker run -d -p 1935:1935 -p 8080:8080 -p 18081:18080 --name nginx-rtmp nginx-rtmp
+docker run -d -p 1935:1935 -p 8080:8080 -p 18081:18080 -v $(pwd)/rec:/tmp/rec --name nginx-rtmp nginx-rtmp
