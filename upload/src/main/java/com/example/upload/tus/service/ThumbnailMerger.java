e74ec8d5a7de6d2410f75067d9403ec034c2778e
haerong22 <haerong22@naver.com>
2023-06-10 09:50:13 +0900

    #7 upload: thumbnail merger

--- upload/src/main/java/com/example/upload/tus/service/ThumbnailMerger.java
+++ upload/src/main/java/com/example/upload/tus/service/ThumbnailMerger.java
@@ -11,44 +11,56 @@ import java.io.IOException;
 @Slf4j
 public class ThumbnailMerger {
 
-    public static void merge(String filePath, String vodName, long duration) {
+    public static void merge(String filePath, String vodKey) {
 
-        String path = filePath + "/" + vodName;
+        String path = filePath + "/" + vodKey;
 
-        System.out.println("path = " + path);
+        File directory = new File(path);
 
         try {
-            // 첫 번째 이미지 파일을 기준으로 너비와 높이를 가져옴
-            BufferedImage baseImage = ImageIO.read(new File(path + "/"  + vodName + "_0.png"));
-            int width = baseImage.getWidth();
-            int height = baseImage.getHeight();
+            int width = 256;
+            int height = 144;
+
+            int length = directory.listFiles().length;
 
             // 합쳐진 이미지를 저장할 BufferedImage 생성
-            BufferedImage mergedImage = new BufferedImage(width * 3, height, BufferedImage.TYPE_INT_RGB);
+            BufferedImage mergedImage = new BufferedImage(width * 5, height * 5, BufferedImage.TYPE_INT_RGB);
 
             // Graphics2D 객체를 사용하여 이미지를 합침
             Graphics2D g2d = mergedImage.createGraphics();
-            g2d.setColor(java.awt.Color.WHITE); // 배경 색상 설정
-            g2d.fillRect(0, 0, width, height); // 배경 색상으로 이미지를 채움
+            g2d.setColor(Color.BLACK); // 배경 색상 설정
+            g2d.fillRect(0, 0, width * 5, height * 5); // 배경 색상으로 이미지를 채움
 
-            int x = 0;
-            for (long i = 0; i < duration; i++) {
-                String imagePath = path + "/" + vodName + "_" + i + ".png";
-                BufferedImage image = ImageIO.read(new File(imagePath));
-                g2d.drawImage(image, x, 0, null);
-                x += image.getWidth();
+            int num = 0;
+
+            for (int i = 1; i <= length; i++) {
+                int col = Math.max(0, i - (num * 25) - 1) % 5;
+                int row = Math.max(0, i - (num * 25) - 1) / 5;
+                int x = width * col;
+                int y = height * row;
+                System.out.println(i + " ==> (" + x + ", " + y + ")");
+
+                File file = new File(path + "/" + vodKey + "_" + (i - 1) + ".png");
+                BufferedImage image = ImageIO.read(file);
+
+                g2d.drawImage(image, x, y, width, height, null);
+                image.flush();
+
+                if (i % 25 == 0 || i == length) {
+                    // 합쳐진 이미지를 저장
+                    File outputFile = new File(filePath, vodKey + "_" + num + ".png");
+                    ImageIO.write(mergedImage, "png", outputFile);
+                    num++;
+                    g2d.setColor(Color.BLACK); // 배경 색상 설정
+                    g2d.fillRect(0, 0, width * 5, height * 5); // 배경 색상으로 이미지를 채움
+                    log.info("이미지 합치기 완료 ===> {}", outputFile.getPath());
+                }
             }
 
+            mergedImage.flush();
             g2d.dispose(); // Graphics2D 객체 해제
-
-            // 합쳐진 이미지를 저장
-            File outputFile = new File(path + "_m.png");
-            System.out.println("outputFile = " + outputFile);
-            ImageIO.write(mergedImage, "png", outputFile);
-
-            log.info("이미지 합치기가 완료되었습니다.");
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("이미지 합치기 실패 ===> ", e);
         }
     }
 }
