78dd05b562ccd5fd34fcf44d3ce8e3c2850334b5
haerong22 <haerong22@naver.com>
2023-06-24 18:37:47 +0900

    #7 upload: fix thumbnail merger

--- upload/src/main/java/com/example/upload/tus/service/ThumbnailExtractor.java
+++ upload/src/main/java/com/example/upload/tus/service/ThumbnailExtractor.java
@@ -8,6 +8,7 @@ import org.jcodec.common.model.Picture;
 import org.jcodec.scale.AWTUtil;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -44,21 +45,35 @@ public class ThumbnailExtractor {
         return thumbnail.getName();
     }
 
-    public static String extract(File source, long second) throws IOException {
+    public static String extract(File source, long second, long duration) throws IOException {
         String filename = source.getName().split("\\.")[0];
         File thumbnail = new File(source.getParent() + "/" + filename, filename + "_" + second + "." + EXTENSION);
 
+        if (!thumbnail.getParentFile().exists()) {
+            thumbnail.getParentFile().mkdirs();
+        }
+
         try {
+            int width = 256;
+            int height = 144;
+
             FrameGrab frameGrab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(source));
 
             frameGrab.seekToSecondPrecise(second);
 
             Picture picture = frameGrab.getNativeFrame();
 
-            BufferedImage bi = AWTUtil.toBufferedImage(picture);
-            ImageIO.write(bi, EXTENSION, thumbnail);
+            BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
+            BufferedImage bi = AWTUtil.toBufferedImage(picture);
+            Graphics2D g2d = resized.createGraphics();
+            g2d.drawImage(bi, 0, 0, width, height, null);
+            ImageIO.write(resized, EXTENSION, thumbnail);
+
+            resized.flush();
+            bi.flush();
         } catch (Exception e) {
+            e.printStackTrace();
             File defaultImage = new File(DEFAULT_IMAGE_PATH);
 
             try {
@@ -66,6 +81,10 @@ public class ThumbnailExtractor {
             } catch (Exception ex) {
                 log.info("Thumbnail Extract Failed => {}", source.getPath(), e);
             }
+        }
+
+        if (second == duration) {
+            ThumbnailMerger.merge(source.getParent(), filename);
         }
 
         return thumbnail.getName();
